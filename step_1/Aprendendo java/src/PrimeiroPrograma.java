3e7b15da5fe3c032a4d3db7a7cf3823f4dbe1de1
Samuel Lopes <140510892+SamuelLopesAnd@users.noreply.github.com>
2024-10-12 19:25:19 -0300

    primeiroprograma

---
+++ step_1/Aprendendo java/src/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+// Pacotes
+package primeiroprograma;
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        System.out.println("olá mundo!");
+    
+    }
+
+    }
+
+// Em Java, as letras maiúsculas e minúsculas têm um papel importante, pois a linguagem é case-sensitive, ou seja, diferencia letras maiúsculas de minúsculas. Aqui estão algumas implicações dessa característica:
+
+//Nomes de Variáveis e Métodos: minhaVariavel e MinhaVariavel são considerados nomes diferentes. Isso significa que você pode ter uma variável chamada minhaVariavel e outra chamada MinhaVariavel na mesma classe.
+
+//Nomes de Classes: Por convenção, os nomes de classes em Java geralmente começam com letra maiúscula, como MinhaClasse. Isso ajuda a distinguir classes de variáveis e métodos.
+
+//Nomes de Pacotes: Normalmente, os nomes de pacotes são escritos em letras minúsculas, como meu.pacote.
+
+//Constantes: As constantes são frequentemente escritas em letras maiúsculas, com palavras separadas por underscores, como MINHA_CONSTANTE.
